fe36222484fd8d45a8d7056005a4e7a29e6dbdb6
river <riverhuang011@gmail.com>
2019-09-24 15:17:58 +0700

    image responsive  javascript

--- src/Components/Javascript/index.js
+++ src/Components/Javascript/index.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react'
-import {Button, Jumbotron,Container, Row, Col, Card, CardImg} from 'reactstrap'
+import {
+    Button,
+    Jumbotron,
+    Container,
+    Row,
+    Col,
+    Card,
+    CardImg
+} from 'reactstrap'
 import webdevelopment from './webdevelopment.jpg'
 import "react-image-gallery/styles/css/image-gallery.css"
-import ImageGallery  from 'react-image-gallery'
+import ImageGallery from 'react-image-gallery'
 import wlpp1 from './wlpp1.jpg'
 import wlpp2 from './wlpp2.png'
 import wlpp3 from './wlpp3.jpg'
@@ -11,24 +19,21 @@ import Mern from './Mern.jpg'
 import javascriptsallary from './javascriptsallary.png'
 import javascriptframework from './javascriptframework.jpg'
 import javascriptneed from './'
-export default class javascript extends Component{
-    render(){
+export default class javascript extends Component {
+    render() {
         const images = [
             {
-                original:wlpp1,
-                thumbnail:wlpp1,
-            },
-            {
-                original:wlpp2,
-                thumbnail:wlpp2,
-            },
-            {
-                original:wlpp3,
-                thumbnail:wlpp3,
-            },
-            {
-                original:wlpp4,
-                thumbnail:wlpp4,
+                original: wlpp1,
+                thumbnail: wlpp1
+            }, {
+                original: wlpp2,
+                thumbnail: wlpp2
+            }, {
+                original: wlpp3,
+                thumbnail: wlpp3
+            }, {
+                original: wlpp4,
+                thumbnail: wlpp4
             }
         ]
         let test = `what we can do by using javascript: \n
@@ -40,37 +45,50 @@ export default class javascript extends Component{
 
         but before learning javascript you must learning HTML and CSS for the UI(User Interface)
         by learning it all you can even make you own website`
-        return(
+        return (
             <div>
-                <Jumbotron fluid style={{backgroundImage:`url('${Mern}')`, backgroundSize:'cover', padding:'9%'}}>
-                    <h1 style={{color:'white'}}>Javascript</h1>
+                <Jumbotron
+                    fluid
+                    style={{
+                    backgroundImage: `url('${Mern}')`,
+                    backgroundSize: 'cover',
+                    padding: '10%'
+                }}>
+                    <h1 style={{
+                        color: 'white'
+                    }}>Javascript</h1>
                 </Jumbotron>
-                
+
                 <Container className="text-center mt-4">
                     <h1>Intro what is Javascript</h1>
-                <h5>
-                {test}
-                </h5>
-                <h5>JavaScript allows you to build interactive websites. JavaScript has become an essential web technology along with HTML and CSS, as most browsers implement JavaScript. Thus, You must learn JavaScript if you want to get into web development, 
-                    and you must learn it well if you're planning on being a front-end developer or on using JavaScript for backend development.
-                    Furthermore, JavaScript usage has now extended to mobile app development, desktop app development, and game development. 
-                    All in all, it has exploded in popularity and is now a very useful skill to learn.
-                </h5>
-                <h1>Example website UI</h1>
-                <div className="mt-4">
-                <ImageGallery items={images} />
-                </div>
-                <div className="mt-4">
-                    this is the website template that people make by using HTML, CSS, and Javascript
-                </div>
-                <Card></Card>
-                <Card></Card>
-                <h1>javascript Framework</h1>
-                <img src={javascriptframework}/>
-                
-                <h1>Career Opportunities</h1>
-                <img src={javascriptsallary}/>
-                </Container>    
+                    <h5>
+                        {test}
+                    </h5>
+                    <h5>JavaScript allows you to build interactive websites. JavaScript has become
+                        an essential web technology along with HTML and CSS, as most browsers implement
+                        JavaScript. Thus, You must learn JavaScript if you want to get into web
+                        development, and you must learn it well if you're planning on being a front-end
+                        developer or on using JavaScript for backend development. Furthermore,
+                        JavaScript usage has now extended to mobile app development, desktop app
+                        development, and game development. All in all, it has exploded in popularity and
+                        is now a very useful skill to learn.
+                    </h5>
+                    <h1>Example website UI</h1>
+                    <div className="mt-4">
+                        <ImageGallery items={images}/>
+                    </div>
+                    <div className="mt-4">
+                        this is the website template that people make by using HTML, CSS, and Javascript
+                    </div>
+                    <Card></Card>
+                    <Card></Card>
+                    <h1>javascript Framework</h1>
+                    <img src={javascriptframework} width="100%"/>
+
+                    <h1>Career Opportunities</h1>
+
+                    <img src={javascriptsallary} width="100%"/>
+                </Container>
             </div>
         )
     }
